197389ea2677edaf1bc562669ae1fd076a737c5d
bhclira <brenohencastro@gmail.com>
2025-01-04 12:12:34 -0300

    integrando a poke api ini

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
-console.log ('Sucesso!');
+
+const offset = 0;
+const limit = 10;
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10';
+
+function convertPokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">Bulbasaur</span>
+
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Bulbasaur">
+
+            </div>
+                    
+        </li>
+    `  
+}
+
+fetch(url)
+
+.then((response) => response.json())
+
+.then((jsonBody) => jsonBody.results)
+
+.then((pokemonList) => {
+    debugger
+    console.log(pokemonList)
+})
+
+.catch((error) => console.error(error))
